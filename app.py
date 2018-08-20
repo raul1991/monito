f56705fb40a6fe45511a2cd36e2dde146cf4dcce
Raman Mangla <ramanmangla7@gmail.com>
2018-08-20 11:38:47 +0530

    Add relevant project files

---
+++ app.py
@@ -0,0 +1,161 @@
+
+import os
+
+from flask import Flask, render_template, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Getting absolute path for directory, __file__ = module's name (app.py)
+projectDir = os.path.dirname(os.path.abspath(__file__))
+
+# Path for database in app's directory
+databaseFile = "sqlite:///{}".format(os.path.join(projectDir, "Monito.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = databaseFile
+app.config["SECRET_KEY"] = os.urandom(24)
+app.config["PERMANENT_SESSION_LIFETIME"] = 600 # 300 seconds
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column('userID', db.Integer, primary_key = True)
+	name = db.Column(db.String(30))
+	username = db.Column(db.String(20), unique = True)
+	email = db.Column(db.String(30), unique = True)
+	vdaIP = db.Column(db.String(15), unique = True)
+
+	def __repr__(self):
+		return ("<Name: {}>".format(self.name) + 
+		"<Username: {}>".format(self.username) + 
+		"<Email: {}>".format(self.email) + 
+		"<VDA-IP: {}>".format(self.vdaIP))
+
+class Machine(db.Model):
+	id = db.Column('machineID', db.Integer, primary_key = True)
+	IP = db.Column(db.String(15), unique = True)
+	vdaIPs = db.Column(db.String(50))
+
+	def __repr__(self):
+		return ("<Machine IP: {}".format(self.IP) + " VDA-IPs: {}>".format(self.vdaIPs))
+
+
+@app.route('/', methods = ["GET", "POST"])
+def login():
+	users = User.query.all()
+	session.clear()
+
+	if request.form:
+		userName = request.form.get('username').lower()
+
+		for user in users:
+			if user.username == userName:
+				session['user_name'] = user.username
+				return redirect('/dashboard')
+
+		return render_template('login.html', SERVER_ERROR = 'Oops! Incorrect username.')
+
+	return render_template('login.html')
+
+@app.route('/register', methods = ["GET", "POST"])
+def register():
+	users = User.query.all()
+	userExists = False
+
+	if request.form:
+
+		user = User(name = request.form.get('user_name').lower(), 
+			username = request.form.get('username').lower(), 
+			email = request.form.get('user_email').lower(),
+			vdaIP = request.form.get('user_vda-ip'))
+
+		for elem in users:
+			if elem.username == user.username or elem.email == user.email or elem.vdaIP == user.vdaIP:
+				userExists = True
+
+		if not userExists:
+			db.session.add(user)
+			db.session.commit()
+
+			return redirect('/')
+
+		return render_template('register.html', SERVER_ERROR = 'Oops! Something went wrong!')
+
+	return render_template('register.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/dashboard')
+def dashboard():
+	if isUserLoggedIn():
+		username = session['user_name']
+		currentUser = User.query.filter_by(username = username).first()
+
+		return render_template('dashboard.html', currentUser = currentUser)
+	else:
+		return redirect('/')
+
+@app.route('/mapping', methods = ["POST"])
+def mapping():
+	if request.form:
+		machine = Machine(vdaIPs = request.form.get('vda_ips'), 
+				IP = request.form.get('machine_ip'))
+		dbMachine = Machine.query.filter_by(IP = machine.IP).first()
+		
+		if dbMachine:
+			dbMachine.vdaIPs = machine.vdaIPs
+			db.session.commit()
+			return 'Updated Machine'
+		else:
+			db.session.add(machine)
+			db.session.commit()
+			return 'Added Machine'
+
+@app.route('/mappings')
+def mappings():
+	machines = Machine.query.all()
+	users = User.query.all()
+	machinesStr = ''
+
+	for machine in machines:
+		vdaIPs = machine.vdaIPs.split(',')
+		vdaNameMap = ''
+
+		for IP in vdaIPs:
+			user = User.query.filter_by(vdaIP = IP).first()
+
+			if user:
+				vdaNameMap += user.name + ','
+			else:
+				vdaNameMap += IP + ','
+
+		machinesStr += machine.IP + ':' + vdaNameMap[:-1] + ';'
+
+	return machinesStr[:-1]
+
+@app.route('/update', methods = ["POST"])
+def update():
+	if request.form:
+		machine = Machine(vdaIPs = request.form.get('vda_ips'), 
+				IP = request.form.get('machine_ip'))
+
+		db.session.add(machine)
+		db.session.commit()
+
+		return "Added Machine and Users"
+
+
+def isUserLoggedIn():
+	try:
+		return session['user_name'] != None
+	except:
+		return False
+
+if __name__ == "__main__":
+	db.create_all()
+	app.run(host="0.0.0.0")
+
+
+
---
+++ static/main.js
@@ -0,0 +1,89 @@
+
+//Responsive menu
+
+var display = false;
+
+function displayMenu() {
+	var menu = document.querySelector('.menu');
+	var nav = document.querySelector('nav');
+	var bars = document.querySelector('.menu div');
+
+	if(display == false) {
+		nav.style.display = 'block';
+		nav.style.opacity = '0';
+		setTimeout(function() {
+			nav.style.opacity = '1';
+		}, 100);
+
+		display = true;
+
+	} else {
+		nav.style.display = 'none';
+
+		display = false;
+	}
+}
+
+//Display User information on dashboard
+
+function displayInfo(option) {
+	var overlay = document.querySelector('.overlay');
+	
+	if(option == 1) {
+		overlay.classList.add('displayInfo');
+	} else {
+		overlay.classList.remove('displayInfo');
+	}
+}
+
+// Display update User information box
+
+// function updateInfoMenu() {
+// 	var updateMenu = document.querySelector('.updateMenu');
+// 	updateMenu.classList.toggle('displayUpdateMenu');
+// }
+
+//AJAX
+
+function sendRequest(config, action){
+	var xmlRequest = new XMLHttpRequest();
+
+	xmlRequest.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200)
+		{
+			action(this);
+		}
+	}
+
+	xmlRequest.open(config.requestType, config.url, true); // async
+	xmlRequest.send();
+}
+
+function getAllMappings() {
+	setInterval(function() {
+
+		var table = document.querySelector('table');
+		table.innerHTML = '<tr><th>Machine</th><th>User(s)</th></tr>';
+
+		sendRequest({'requestType': 'GET', 'url': '/mappings'}, function(XMLObj) {
+			var response = XMLObj.responseText;
+
+			if(response) {
+				response = response.split(';');
+
+				for(var i = 0; i < response.length; i++) {
+					if(i % 2 == 0) {
+						background = '#fff';
+					} else {
+						background = '#efefef';
+					}
+					
+					table.innerHTML += '<tr style = "background:' + background + ';"><td>' + response[i].split(':')[0] + '</td><td>' + response[i].split(':')[1].split(',').join(', ') + '</td></tr>'
+				}
+			} else {
+				console.log('Empty');
+			}
+		});
+	}, 1000 * 10);
+}
+
