8fe6c6115fcdf24c0bc7b9b5586ca469e04a8ba9
Rahul Bawa <supremeanitabawa@gmail.com>
2018-08-30 16:26:14 +0530

    Improve to the connection logic
    - Changed the way ssh is done.
    - Cleaned up the code
    - Silent ssh enabled
    - options for the command introduced
    	-m : true/false
    	-k : path to ssh key file
    	-f : path to file containing comma separated values
    		supported values are as follows (in the same order)
    		1. hostname
    		2. team name
    		3. username

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in $(cat team.machines)
-do
-	machine=$(echo "${i}" | awk '{split($0,tokens,","); print tokens[1]}')
-	echo $machine
-	ssh-copy-id admin@$machine
-
-done
--- fetch_machines.sh
+++ fetch_machines.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-if [[ -z $1 ]];then
-	echo "Path to config file (*.machines file) is missing";
-	exit
-fi
-machines=`cat $1`
-cmd='who | grep -oE "([a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+)"'
+
+# variables for the script
+key_file="" #for ssh key file. Must the absolute path
+machines="" # for file that consists of host,team,username separated by comma.
+
+cmd='who | grep -oE "([a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+)"' # run the who command and grep on the ip or host name.
 myHostName=`hostname`
+shouldAddMachines="true"
+
+
 function join_by { local IFS="$1"; shift; echo "$*"; }
 function getVisitors
 {
-   	
-   for machine in ${machines}
-   do
-	team=$(echo "${machine}" | awk '{split($0,tokens,","); print tokens[2]}')
-	machine=$(echo "${machine}" | awk '{split($0,tokens,","); print tokens[1]}')
+	machine="$1"
+	team="$2"	
+	user="$3"
 	echo "Running ${cmd} on ${machine}"
 
-	#if [[ ${states["$machine"]} == "" ]];then
 		
-		result=$(ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -t admin@${machine} ${cmd})
+		result=$(ssh -q -i "${key_file}" -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -t ${user}@${machine} ${cmd})
 		isMachineUp=$?
 		ips=()
-		for user in ${result}
+		for visitor in ${result}
 		do
 			shopt -s nocasematch
-			if [[ ! "$user" =~ $myHostName.* ]];then
-				ips+=("${user}")
+			if [[ ! "$visitor" =~ $myHostName.* ]];then
+				ips+=("${visitor}")
 			fi
 			shopt -u nocasematch
 		done
@@ -36,27 +36,18 @@ function getVisitors
 				ips="Free";
 			fi
 		fi
-		echo "Users available on ${machine} => $ips"
+		echo "Users found on $machine === > $ips"
 		addMachine $(join_by , "${ips}") "${machine}" "${team}"
-	#fi
-   done
 }
 
-function addKeys
+function addMachines
 {
-  for m in ${machines}
+  for m in $(cat $1)
   do
-	machine=$(echo "${m}" | awk '{split($0,tokens,","); print tokens[1]}')
-	team=$(echo "${m}" | awk '{split($0,tokens,","); print tokens[2]}')
-	echo "Adding machine ${m} to team $team"
-	addMachine "Free" "$machine" "$team"
-	exits=$(cat ${HOME}/.ssh/known_hosts | grep "${machine}")
-	exists=$?
-	if [[ ${exists} -ne 0 ]];then
-		echo "Running command => ssh-copy-id admin@${machine}"
-		ssh-copy-id admin@${machine}
-		isMachineUp=$?
-	fi
+       machine=$(echo "${m}" | awk '{split($0,tokens,","); print tokens[1]}')
+       team=$(echo "${m}" | awk '{split($0,tokens,","); print tokens[2]}')
+       echo "Adding machine ${m} to team $team"
+       addMachine "Free" "$machine" "$team"
   done
 }
 
@@ -64,12 +55,61 @@ function addMachine
 {
 	# first argument - machine name
 	# second argument - team name
-	curl -X POST http://localhost:5000/mapping -F "owner=$3" -F "vda_ips=$1" -F "machine_ip=$2" > /dev/null
+	curl -X POST http://localhost:5000/mapping -F "owner=$3" -F "vda_ips=$1" -F "machine_ip=$2" > /dev/null&
 }
 
-addKeys
+function showValidOptions
+{
+	echo "-f      -> comma separated file with machine,owner,username"
+	echo "-k      -> absolute path to the ssh key (without the .pub and in double quotes)"
+	echo "-m      -> a flag to tell the script if it should add machines or not in the database. Provide this only if you have not added a new machine"
+	echo "-h      -> to show help for this script"
+
+}
+
+function parseArgs
+{
+	local OPTIND
+	echo "Arguments $@"
+	while getopts ":f:k:m:" opt; do
+	case $opt in
+		f ) # machine file
+			machines="$OPTARG"
+			;;
+		k ) # ssh key file
+			key_file="$OPTARG"
+			;;
+		m) # flag to add machines or not
+			shouldAddMachines="$OPTARG"
+			;;
+		h) # help menu
+			showValidOptions
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument" >&2
+			exit 1
+			;;
+		\?) # error scenario
+			echo "Invalid option -$OPTARG" >&2
+			showValidOptions
+			;;
+	esac
+	done
+	shift $((OPTIND-1))
+}
+
+parseArgs "$@"
+
+[[ $shouldAddMachines == "false" ]] && echo "Machines will not be added" || addMachines "$machines"
+
 while true;
 do
-	getVisitors
-	sleep 5
+	for line in $(cat $machines)
+	do
+		machine=$(echo "$line" | awk '{split($0,arr,",");print arr[1]}')
+		team=$(echo "$line" | awk '{split($0,arr,",");print arr[2]}')
+		user=$(echo "$line" | awk '{split($0,arr,",");print arr[3]}')
+		getVisitors "$machine" "$team" "$user"
+		sleep 5
+	done
 done
