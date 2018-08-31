d800edbb094c6b9a4f5a64f0071663ac9f62186d
Rahul Bawa <supremeanitabawa@gmail.com>
2018-08-31 15:36:56 +0530

    Match hostname without the domain name

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-
 import os
 
 from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
-
 
 # Getting absolute path for directory, __file__ = module's name (app.py)
 projectDir = os.path.dirname(os.path.abspath(__file__))
@@ -14,130 +12,137 @@ databaseFile = "sqlite:///{}".format(os.path.join(projectDir, "Monito.db"))
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = databaseFile
 app.config["SECRET_KEY"] = os.urandom(24)
-app.config["PERMANENT_SESSION_LIFETIME"] = 900 # seconds
+app.config["PERMANENT_SESSION_LIFETIME"] = 900  # seconds
 db = SQLAlchemy(app)
 
-class User(db.Model):
-	id = db.Column('userID', db.Integer, primary_key = True)
-	name = db.Column(db.String(30))
-	username = db.Column(db.String(20), unique = True)
-	vdaIP = db.Column(db.String(15), unique = True)
-	hostname = db.Column(db.String(50), unique = True)
 
-	def __repr__(self):
-		return ("<Name: {}>".format(self.name) + 
-		"<Username: {}>".format(self.username) + 
-		"<VDA-IP: {}>".format(self.vdaIP))
+class User(db.Model):
+    id = db.Column('userID', db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    username = db.Column(db.String(20), unique=True)
+    vdaIP = db.Column(db.String(15), unique=True)
+    hostname = db.Column(db.String(50), unique=True)
+
+    def __repr__(self):
+        return ("<Name: {}>".format(self.name) +
+                "<Username: {}>".format(self.username) +
+                "<VDA-IP: {}>".format(self.vdaIP))
+
 
 class Machine(db.Model):
-	id = db.Column('machineID', db.Integer, primary_key = True)
-	IP = db.Column(db.String(15), unique = True)
-	vdaIPs = db.Column(db.String(200))
-	owner = db.Column(db.String(10))
+    id = db.Column('machineID', db.Integer, primary_key=True)
+    IP = db.Column(db.String(15), unique=True)
+    vdaIPs = db.Column(db.String(200))
+    owner = db.Column(db.String(10))
 
-	def __repr__(self):
-		return ("<Machine IP: {}".format(self.IP) + " VDA-IPs: {}".format(self.vdaIPs) + " Owner: {}>".format(self.owner))
+    def __repr__(self):
+        return (
+        "<Machine IP: {}".format(self.IP) + " VDA-IPs: {}".format(self.vdaIPs) + " Owner: {}>".format(self.owner))
 
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def login():
-	users = User.query.all()
-	session.clear()
+    users = User.query.all()
+    session.clear()
 
-	if request.form:
-		userName = request.form.get('username').lower()
+    if request.form:
+        userName = request.form.get('username').lower()
 
-		for user in users:
-			if user.username == userName:
-				session['user_name'] = user.username
-				return redirect('/dashboard')
+        for user in users:
+            if user.username == userName:
+                session['user_name'] = user.username
+                return redirect('/dashboard')
 
-		return render_template('login.html', SERVER_ERROR = 'Oops! Incorrect username.')
+        return render_template('login.html', SERVER_ERROR='Oops! Incorrect username.')
 
-	return render_template('login.html')
+    return render_template('login.html')
 
-@app.route('/register', methods = ["GET", "POST"])
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
-	users = User.query.all()
-	userExists = False
+    users = User.query.all()
+    userExists = False
 
-	if request.form:
+    if request.form:
 
-		user = User(name = request.form.get('user_name').lower(), 
-			username = request.form.get('username').lower(), 
-			vdaIP = request.form.get('user_vda-ip'),
-			hostname = request.form.get('user_hostname').lower())
+        user = User(name=request.form.get('user_name').strip().lower(),
+                    username=request.form.get('username').strip().lower(),
+                    vdaIP=request.form.get('user_vda-ip').strip(),
+                    hostname=request.form.get('user_hostname').strip().lower())
 
-		for elem in users:
-			if elem.username == user.username or elem.vdaIP == user.vdaIP or elem.hostname == user.hostname:
-				userExists = True
-				break
+        for elem in users:
+            if elem.username == user.username or elem.vdaIP == user.vdaIP or elem.hostname == user.hostname:
+                userExists = True
+                break
 
-		if not userExists:
-			db.session.add(user)
-			db.session.commit()
+        if not userExists:
+            db.session.add(user)
+            db.session.commit()
 
-			return redirect('/')
+            return redirect('/')
 
-		return render_template('register.html', SERVER_ERROR = 'User already exists!')
+        return render_template('register.html', SERVER_ERROR='User already exists!')
 
-	return render_template('register.html')
+    return render_template('register.html')
+
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/dashboard')
 def dashboard():
-	if isUserLoggedIn():
-		username = session['user_name']
-		currentUser = User.query.filter_by(username = username).first()
+    if isUserLoggedIn():
+        username = session['user_name']
+        currentUser = User.query.filter_by(username=username).first()
 
-		return render_template('dashboard.html', currentUser = currentUser)
-	else:
-		return redirect('/')
+        return render_template('dashboard.html', currentUser=currentUser)
+    else:
+        return redirect('/')
 
-@app.route('/mapping', methods = ["POST"])
+
+@app.route('/mapping', methods=["POST"])
 def mapping():
-	if request.form:
-		machine = Machine(vdaIPs = request.form.get('vda_ips'), 
-				IP = request.form.get('machine_ip'),
-				owner = request.form.get('owner').lower())
-		dbMachine = Machine.query.filter_by(IP = machine.IP).first()
-		
-		if dbMachine:
-			dbMachine.vdaIPs = machine.vdaIPs
-			db.session.commit()
-			return 'Updated Machine'
-		else:
-			db.session.add(machine)
-			db.session.commit()
-			return 'Added Machine'
+    if request.form:
+        machine = Machine(vdaIPs=request.form.get('vda_ips'),
+                          IP=request.form.get('machine_ip'),
+                          owner=request.form.get('owner').lower())
+        dbMachine = Machine.query.filter_by(IP=machine.IP).first()
+
+        if dbMachine:
+            dbMachine.vdaIPs = machine.vdaIPs
+            db.session.commit()
+            return 'Updated Machine'
+        else:
+            db.session.add(machine)
+            db.session.commit()
+            return 'Added Machine'
+
 
 @app.route('/mappings')
 def mappings():
-	machines = Machine.query.all()
-	users = User.query.all()
-	machinesStr = ''
+    machines = Machine.query.all()
+    users = User.query.all()
+    machinesStr = ''
 
-	for machine in machines:
-		vdaIPs = machine.vdaIPs.split(',')
-		vdaNameMap = ''
+    for machine in machines:
+        vdaIPs = machine.vdaIPs.split(',')
+        vdaNameMap = ''
 
-		for IP in vdaIPs:
-			userByVDA = User.query.filter_by(vdaIP = IP).first()
-			userByHost = User.query.filter_by(hostname = IP.lower()).first()
+        for IP in vdaIPs:
+            userByVDA = User.query.filter_by(vdaIP=IP).first()
+            userByHost = User.query.filter(User.hostname.ilike(IP.split(".")[0] + "%")).first()
+            if userByVDA:
+                vdaNameMap += userByVDA.name + ','
+            elif userByHost:
+                vdaNameMap += userByHost.name + ','
+            else:
+                vdaNameMap += IP.lower() + ','
 
-			if userByVDA:
-				vdaNameMap += userByVDA.name + ','
-			elif userByHost:
-				vdaNameMap += userByHost.name + ','
-			else:
-				vdaNameMap += IP.lower() + ','
+        machinesStr += machine.IP + ':' + machine.owner + ':' + vdaNameMap[:-1] + ';'
 
-		machinesStr += machine.IP + ':' + machine.owner + ':' + vdaNameMap[:-1] + ';'
-
-	return machinesStr[:-1]
+    return machinesStr[:-1]
 
 # @app.route('/update', methods = ["POST"])
 # def update():
@@ -152,14 +157,12 @@ def mappings():
 
 
 def isUserLoggedIn():
-	try:
-		return session['user_name'] != None
-	except:
-		return False
+    try:
+        return session['user_name'] != None
+    except:
+        return False
+
 
 if __name__ == "__main__":
-	db.create_all()
-	app.run(host="0.0.0.0")
-
-
-
+    db.create_all()
+    app.run(host="0.0.0.0")
