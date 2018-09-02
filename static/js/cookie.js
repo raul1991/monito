8b54ff4bf5a47979e5dbf44f3634fe96490ad0d5
Rahul Bawa <rahul.bawa@ericsson.com>
2018-09-03 00:02:45 +0530

    Refactor the front end code
    Wrapped everything inside a module. Only the public functions have
    now been exposed.
    
    fixes #12

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -1,34 +1,31 @@
-function setCookie(cname,cvalue) {
-    var d = new Date();
-    d.setTime(d.getTime() + (365*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
+var _cookies = (function(){
+    var get = (function (key) {
+        var name = key + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+        return "";
+    });
+    var find = (function (key) {
+        return getCookie(key) || "";
+    });
+    var update = (function (key, value) {
+        var d = new Date();
+        d.setTime(d.getTime() + (365*24*60*60*1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = key + "=" + value + ";" + expires + ";path=/";
+    });
+    return {
+        get : get,
+        find : find,
+        update : update
     }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
-    }
-}
+})();
--- static/js/main.js
+++ static/js/main.js
@@ -1,161 +1,150 @@
-
 //Responsive menu
+var monito = (function (cookiesModule) {
+    var display = false;
+    var autoRefreshElRef = document.getElementById('toggleAutoRefresh');
+    var refreshIntervalId; // used to store the refresh interval id in order to clear it later on.
+    var prefs = {
+        'refreshInterval': 'prefs.refresh'
+    };
+    var autoRefresh = (function (cookies) {
+        var update = (function (state) {
+            autoRefreshElRef.innerHTML = state;
+            cookies.update(prefs.refreshInterval, state);
+            toggle(state);
+        });
+        var toggle = (function (state) {
+            autoRefreshElRef.innerHTML = (state.toLocaleLowerCase() === 'enable') ? 'Disable' : 'Enable';
+            reactToState(state);
+        });
+        var reactToState = (function (state) {
+            if (state.toLocaleLowerCase() === 'enable' && !refreshIntervalId) {
+                refreshIntervalId = window.setInterval(getAllMappings, 1000 * 10);
+            }
+            else {
+                window.clearInterval(refreshIntervalId);
+                refreshIntervalId = undefined;
+            }
+        });
+        var state = (autoRefreshElRef && autoRefreshElRef.innerHTML) || 'Disable';
+        return {
+            state: state,
+            update: update
+        }
+    })(cookiesModule);
 
-var display = false;
-var showAlert = false;
-var alertBox = document.getElementById('alertBox');
-var refreshIntervalId; // used to store the refresh interval id in order to clear it later on.
-var cookies = {
-    'refreshInterval': 'prefs.refresh'
-}
-function restorePreferences()
-{
-    setPreferences(getCookie(cookies.refreshInterval));
-}
-
-function setPreferences(state)
-{
-    state.toLocaleLowerCase() === "enable" ? onRefreshEnabled() : onRefreshDisabled();
-}
-
-function displayMenu() {
-	var menu = document.querySelector('.menu');
-	var nav = document.querySelector('nav');
-	var bars = document.querySelector('.menu div');
-
-	if(display == false) {
-		nav.style.display = 'block';
-		nav.style.opacity = '0';
-		setTimeout(function() {
-			nav.style.opacity = '1';
-		}, 100);
-
-		display = true;
-
-	} else {
-		nav.style.display = 'none';
-
-		display = false;
-	}
-}
-
-//Display User information on dashboard
-
-function displayInfo(option) {
-	var overlay = document.querySelector('.overlay');
-	
-	if(option == 1) {
-		overlay.classList.add('displayInfo');
-	} else {
-		overlay.classList.remove('displayInfo');
-	}
-}
-
-// Display update User information box
-
-// function updateInfoMenu() {
-// 	var updateMenu = document.querySelector('.updateMenu');
-// 	updateMenu.classList.toggle('displayUpdateMenu');
-// }
-
-//AJAX
-
-function sendRequest(config, action){
-	var xmlRequest = new XMLHttpRequest();
-
-	xmlRequest.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200)
-		{
-			action(this);
-		}
-	}
-
-	xmlRequest.open(config.requestType, config.url, true); // async
-	xmlRequest.send();
-}
-
-function onRefreshEnabled()
-{
-    if (!refreshIntervalId)
-    {
-        refreshIntervalId = setInterval(getAllMappings, 1000 * 10); // restarting the function.
+    function restorePreferences() {
+        autoRefresh.update(cookiesModule.get(prefs.refreshInterval));
     }
-}
 
-function onRefreshDisabled()
-{
-    window.clearInterval(refreshIntervalId);
-    refreshIntervalId = undefined;
-}
+    function displayMenu() {
+        var menu = document.querySelector('.menu');
+        var nav = document.querySelector('nav');
+        var bars = document.querySelector('.menu div');
 
-function toggleAutoRefresh(element)
-{
-    var currentState = element && element.innerHTML;
-    element.innerHTML = (currentState.toLocaleLowerCase === "enable") ? "Disable" : "Enable";
-    if (currentState.toLocaleLowerCase() === "enable")
-    {
-        element.innerHTML = "Disable";
-        onRefreshEnabled(element);
+        if (display === false) {
+            nav.style.display = 'block';
+            nav.style.opacity = '0';
+            setTimeout(function () {
+                nav.style.opacity = '1';
+            }, 100);
+
+            display = true;
+
+        } else {
+            nav.style.display = 'none';
+
+            display = false;
+        }
     }
-    else
-    {
-        // disabling it.
-        element.innerHTML = "Enable";
-        onRefreshDisabled(element);
+
+    //Display User information on dashboard
+
+    function displayInfo(option) {
+        var overlay = document.querySelector('.overlay');
+
+        if (option === 1) {
+            overlay.classList.add('displayInfo');
+        } else {
+            overlay.classList.remove('displayInfo');
+        }
     }
-    setCookie(cookies.refreshInterval, element.innerHTML);
-}
 
-function getAllMappings() {
-    var table = document.querySelector('table');
-    sendRequest({'requestType': 'GET', 'url': '/mappings'}, function(XMLObj) {
-        var response = XMLObj.responseText;
+    // Display update User information box
 
-        if(response) {
-            response = response.split(';');
+    // function updateInfoMenu() {
+    // 	var updateMenu = document.querySelector('.updateMenu');
+    // 	updateMenu.classList.toggle('displayUpdateMenu');
+    // }
 
-            for(var i = 0; i < response.length; i++) {
-                var row = document.createElement('tr');
-                var formattedData = getFormattedData(response[i]);
-                row.id = formattedData.machine;
-                if (!machineExists(row.id))
-                {
-                    for (k in formattedData)
-                    {
-                        if (formattedData.hasOwnProperty(k))
-                        {
-                            var data = document.createElement('td');
-                            data.innerHTML = formattedData[k];
-                            row.appendChild(data);
-                        }
-                    }
-                    table.appendChild(row);
-                }
-                else
-                {
-                    updateUsersData(formattedData);
-                }
+    //AJAX
+
+    function sendRequest(config, action) {
+        var xmlRequest = new XMLHttpRequest();
+
+        xmlRequest.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                action(this);
             }
         }
-    });
-}
 
-function updateUsersData(data)
-{
-    document.getElementById(data.machine).children[2].innerHTML = data.users;
-}
+        xmlRequest.open(config.requestType, config.url, true); // async
+        xmlRequest.send();
+    }
 
-function machineExists(id)
-{
-    return document.getElementById(id);
-}
+    function getAllMappings() {
+        var table = document.querySelector('table');
+        sendRequest({'requestType': 'GET', 'url': '/mappings'}, function (XMLObj) {
+            var response = XMLObj.responseText;
 
-function getFormattedData(response)
-{
-    var id = response.split(':')[0];
-    var obj = {};
-    obj['machine'] = id;
-    obj['owner'] = response.split(':')[1];
-    obj['users'] = response.split(':')[2].split(',').join(', ');
-    return obj;
-}
+            if (response) {
+                response = response.split(';');
+
+                for (var i = 0; i < response.length; i++) {
+                    var row = document.createElement('tr');
+                    var formattedData = getFormattedData(response[i]);
+                    row.id = formattedData.machine;
+                    if (!machineExists(row.id)) {
+                        for (k in formattedData) {
+                            if (formattedData.hasOwnProperty(k)) {
+                                var data = document.createElement('td');
+                                data.innerHTML = formattedData[k];
+                                row.appendChild(data);
+                            }
+                        }
+                        table.appendChild(row);
+                    }
+                    else {
+                        updateUsersData(formattedData);
+                    }
+                }
+            }
+        });
+    }
+
+    function updateUsersData(data) {
+        document.getElementById(data.machine).children[2].innerHTML = data.users;
+    }
+
+    function machineExists(id) {
+        return document.getElementById(id);
+    }
+
+    function getFormattedData(response) {
+        var id = response.split(':')[0];
+        var obj = {};
+        obj['machine'] = id;
+        obj['owner'] = response.split(':')[1];
+        obj['users'] = response.split(':')[2].split(',').join(', ');
+        return obj;
+    }
+
+    function init() {
+        getAllMappings();
+        restorePreferences();
+    }
+
+    return {
+        init: init,
+        refresh: autoRefresh
+    }
+})(_cookies);
