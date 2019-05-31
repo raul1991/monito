8ba811ea9f3ad70305a75812d8c20aac096800e5
Rahul Bawa <rahul.bawa@ericsson.com>
2019-05-31 10:02:02 +0530

    Add support for multiple recipients using sendmail

--- send_mail.sh
+++ send_mail.sh
@@ -2,7 +2,7 @@
 
 function send_email()
 {
-    cat "$1" | sed -e s/'${owner}'/"$2"/g -e s/'${active_users}'/"$4"/g -e s/'${machine}'/"$3"/g | curl -s --url 'smtps://smtp.gmail.com:465' --ssl-reqd  --mail-rcpt "$5" --upload-file - --user "$6" --insecure
+  cat "$1" | sed -e s/'${owner}'/"$2"/g -e s/'${active_users}'/"$4"/g -e s/'${machine}'/"$3"/g -e s/'${recipients}'/"$5"/g | /usr/sbin/sendmail -t
 }
 
 template_file="$1"
@@ -10,6 +10,5 @@ owner="$2"
 machine="$3"
 active_users="$4"
 recipient="$5"
-creds="$6"
 
-send_email "${template_file}" "${owner}" "${machine}" "${active_users}" "${recipient}" "${creds}"
+send_email "${template_file}" "${owner}" "${machine}" "${active_users}" "${recipient}"
