969d776d243f61ef3a6eb23bdf7e22834d5b5c7a
Rahul Bawa <rahul.bawa@ericsson.com>
2020-10-04 00:33:47 +0530

    Fix email sending logic

--- send_mail.sh
+++ send_mail.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+. ./.env
+USE_GMAIL=${USE_GMAIL:-false}
 function send_email()
 {
-  cat "$1" | sed -e s/'${owner}'/"$2"/g -e s/'${active_users}'/"$4"/g -e s/'${machine}'/"$3"/g -e s/'${recipients}'/"$5"/g | /usr/sbin/sendmail -t
+  if [[ ${USE_GMAIL} == "true" ]]; then
+      echo "Using gmail smtp server"
+      cat "$1" | sed -e s/'${owner}'/"$2"/g -e s/'${active_users}'/"$4"/g -e s/'${machine}'/"$3"/g | curl -s --url 'smtps://smtp.gmail.com:465' --ssl-reqd  --mail-rcpt "$5" --upload-file - --user "$(echo ${GMAIL_CREDS} | base64 -d)" --insecure
+  else
+      cat "$1" | sed -e s/'${owner}'/"$2"/g -e s/'${active_users}'/"$4"/g -e s/'${machine}'/"$3"/g -e s/'${recipients}'/"$5"/g | /usr/sbin/sendmail -t
+  fi
 }
 
 template_file="$1"
